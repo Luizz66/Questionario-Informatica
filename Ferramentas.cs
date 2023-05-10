ff771da47b4dd6fc0ce13a379975a296f897517b
Luizz66 <luizgustavoka66@gmail.com>
2023-05-10 19:02:41 -0300

    Ferramentas

---
+++ Ferramentas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Informática
+{
+    public static class Ferramentas
+    {
+        public static void ColorLine(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+        public static void ColorBack(string text, ConsoleColor color)
+        {
+            Console.BackgroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+    }
+}
--- Prova.cs
+++ Prova.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using static Colorir.Color;
+using static Informática.Ferramentas;
 
 namespace Informática
 {
